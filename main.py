2d82cbd8643055d6469e1eba2b231c3414679629
unknown <tristanw3@outlook.com>
2023-11-25 21:41:18 +1100

    -

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ def get_csv_data(filepath: str):
 
 def match_tables(table_1: list, table_2: list):
     if(table_1 == table_2):
-        print("Matched")
+        print('Matched')
+        print('Number of Rows - ' + len(table_1))
+    else:
+        print('Not Matched')
 
 if __name__ == '__main__':
-    filepath_1 = './test_data.csv'
+    filepath_1 = './tests/test_data_1.csv'
     table_1 = get_csv_data(filepath_1)
 
-    filepath_2 = './test_data.csv'
+    filepath_2 = './tests/test_data_2.csv'
     table_2 = get_csv_data(filepath_2)
     
     match_tables(table_1, table_2)
